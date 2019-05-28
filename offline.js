972b4519ad0164b64a94e8d4847e3c9f19c98d1e
semicon <30399464+semicon@users.noreply.github.com>
2019-05-28 07:07:28 +0700

    vdolist
    vdolist

---
+++ offline.js
@@ -0,0 +1,23 @@
+﻿{
+	"version": 1559000452,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/images-sheet0.png",
+		"images/images-sheet1.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"vdo1.mp4",
+		"vdo1.webm",
+		"vdo2.mp4",
+		"vdo2.webm",
+		"vdo3.mp4",
+		"vdo3.webm"
+	]
+}
